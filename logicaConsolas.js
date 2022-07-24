50b32dd98b6ea4f9fdacdff22a94d00d4be5ba04
Camilo España <javiercamilo08@gmail.com>
2022-07-23 22:09:06 -0500

    Agregando carrito con funciones

--- logicaConsolas.js
+++ logicaConsolas.js
@@ -1,9 +1,10 @@
 //After clase 10
-let carrito=[];
+let carrito= [];
 if(localStorage.getItem("carrito")!=null){
     carrito=JSON.parse(localStorage.getItem("carrito"));
     //cargarlos en la tabla - tarea!!!
 }
+let productosJSON = [];
 let lista=document.getElementById("listaConsolas");
     
 //llamada a renderizar
@@ -24,6 +25,8 @@ function renderizarProductos() {
         <button class='btn btn-danger'id='btn${consola.id}'>Comprar</button>
         </li>`;
     }
+    
+
     //eventos boton
     Consolas.forEach(consola=>{
         document.getElementById(`btn${consola.id}`).addEventListener('click',function(){
@@ -41,6 +44,51 @@ function renderizarProductos() {
     });
 }
 
+
+function eliminar(id){
+    let indice=carrito.findIndex(prod => prod.id==id);
+    carrito.splice(indice,1);//eliminando del carro
+    let fila=document.getElementById(`fila${id}`);
+    document.getElementById("tablabody").removeChild(fila);//eliminando de la tabla
+    document.getElementById("gastoTotal").innerText=(`Total: $ ${calcularTotal()}`);
+    localStorage.setItem("carrito",JSON.stringify(carrito));
+}
+
+function agregarACarrito(productoNuevo) {
+    let encontrado = carrito.find(p => p.id == productoNuevo.id);
+    console.log(encontrado);
+    if (encontrado == undefined) {
+        let prodACarrito = new ProductoCarrito(productoNuevo);
+        carrito.push(prodACarrito);
+        console.log(carrito);
+        Swal.fire(
+            'Nuevo producto agregado al carro',
+            productoNuevo.nombre,
+            'success'
+        );
+        //agregamos una nueva fila a la tabla de carrito
+        document.getElementById("tablabody").innerHTML+=(`
+            <tr id='fila${prodACarrito.id}'>
+            <td> ${prodACarrito.id} </td>
+            <td> ${prodACarrito.nombre}</td>
+            <td id='${prodACarrito.id}'> ${prodACarrito.cantidad}</td>
+            <td> ${prodACarrito.precio}</td>
+            <td> <button class='btn btn-light' onclick='eliminar(${prodACarrito.id})'>🗑️</button>`);
+    } else {
+        //pido al carro la posicion del producto 
+        let posicion = carrito.findIndex(p => p.id == productoNuevo.id);
+        console.log(posicion);
+        carrito[posicion].cantidad += 1;
+        //con querySelector falla
+        document.getElementById(productoNuevo.id).innerHTML=carrito[posicion].cantidad;
+    }
+    document.getElementById("gastoTotal").innerText=(`Total: $ ${calcularTotal()}`);
+    localStorage.setItem("carrito",JSON.stringify(carrito));
+
+    document.getElementById("gastoTotal").innerText=(`Total: $ ${calcularTotal()}`);
+    localStorage.setItem("carrito",JSON.stringify(carrito));
+}
+
 function agregarAlCarrito(productoNuevo){
     carrito.push(productoNuevo);
     console.log(carrito);
@@ -52,4 +100,12 @@ function agregarAlCarrito(productoNuevo){
         </tr>
     `;
     localStorage.setItem("carrito",JSON.stringify(carrito));
+}
+
+function calcularTotal() {
+    let suma = 0;
+    for (const elemento of carrito) {
+        suma = suma + (elemento.precio * elemento.cantidad);
+    }
+    return suma;
 }
